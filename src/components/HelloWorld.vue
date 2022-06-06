89c1850d8d025f478634618646e0c9f94a9243e1
Chr1sInTheHouse <bpploabc@gmail.com>
2022-06-06 22:24:35 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="light-green">
+    <v-row class="text-center d-flex justify-center mb-6">
+      <v-col cols="12">
+        <!-- <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        /> -->
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to ponzi-detector !
+        </h1>
+      </v-col>
+      <v-responsive width="400">
+          <v-textarea
+            v-model="opcode"
+            color="cyan darken"
+            label="Paste your opcode here"
+            placeholder="Start typing..."
+            row-height="10"
+            rows="20"
+            solo
+          >
+          </v-textarea>
+          <v-btn @click="submit">submit</v-btn>
+      </v-responsive>
+
+      <v-responsive width="400" min-height="400" class="d-flex align-center">
+        <div>
+          <v-progress-circular
+            indeterminate
+            color="primary"
+          ></v-progress-circular>
+          <h1> Score: {{ score }}</h1>
+          <h1 v-if="ponzi">true</h1>
+          <h1 v-if="!ponzi">false</h1>
+        </div>
+      </v-responsive>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    opcode: "",
+    ponzi: true,
+    score: 0,
+  }),
+
+  methods: {
+    submit() {
+      if (!this.opcode) {
+        return;
+      }
+      this.ponzi = !this.ponzi;
+      this.score = 12;
+    },
+    submit_real() {
+      if (!this.opcode) {
+        return;
+      }
+      axios
+        .post("url", this.opcode, {
+          headers: {
+            "Content-Type": "text/plain",
+          },
+        })
+        .then((res) => {
+          // this.ponzi = !this.ponzi
+          // this.score = 12
+          this.ponzi = res.ponzi;
+          this.score = res.score;
+        });
+    },
+  },
+};
+</script>
